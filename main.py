32f1b3b119d5fb18af8eef4dc0ab45c1a67c78bf
fountesh <128893715+fountesh@users.noreply.github.com>
2024-08-11 14:40:53 +0300

    Update for main

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from fastapi import FastAPI
-import uvicorn
-from typing import List 
 from pydantic import BaseModel
+from typing import List, Any
+import uvicorn
 
-app = FastAPI
 
-users: List[any] = []
+app = FastAPI()
+
+users: List[Any] = []
 
 class User(BaseModel):
-    id:int
+    id: int
     username:str
-    email:str
+    email: str
+    
 
 @app.get("/users", response_model=list[User])
-def get_all_users(user_list=users):
+def get_all_users():
     
     return users
 
+
 @app.get("/users/{user_id}", response_model=User)
-def get_user_by_id(user_id:int):
+def get_user_by_id(user_id: int):
     
-    return users[user_id]
+    return users[user_id]    
+
+
+@app.post("/create_user", response_model=User)
+def create_user(user: User):
+    users.append(user)
+    
+    return user
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=1000, reload=True)
+    uvicorn.run("my_app:app", host="localhost", port=1000, reload=True)
